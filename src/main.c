2dba227dd938156e3410e3416590255401982796
Pierina Camarena <pierinaacam@gmail.com>
2022-07-08 19:10:06 +0200

    starting motor

--- src/main.c
+++ src/main.c
@@ -12,26 +12,26 @@
 
 #include "../includes/minishell.h"
 
-int main(int ac, char **av, char **env)
-{
-    if (ac >= 2)
-    {
-        if (ft_strcmp(av[1], "echo") == 0)
-            ft_echo(av);
-        else if (ft_strcmp(av[1], "pwd") == 0)
-            ft_pwd();
-        else if (ft_strcmp(av[1], "env") == 0)
-            ft_env(env);
-        else
-            {
-                printf("Run it like ./a.out echo [input]\n");
-                printf("Run it like ./a.out pwd\n");
-            }
+// int main(int ac, char **av, char **env)
+// {
+//     if (ac >= 2)
+//     {
+//         if (ft_strcmp(av[1], "echo") == 0)
+//             ft_echo(av);
+//         else if (ft_strcmp(av[1], "pwd") == 0)
+//             ft_pwd();
+//         else if (ft_strcmp(av[1], "env") == 0)
+//             ft_env(env);
+//         else
+//             {
+//                 printf("Run it like ./a.out echo [input]\n");
+//                 printf("Run it like ./a.out pwd\n");
+//             }
 
-    }
-    else
-    {
-        printf("run it like ./minishell echo [input]\n");
-    }
-    return (0);
-}
+//     }
+//     else
+//     {
+//         printf("run it like ./minishell echo [input]\n");
+//     }
+//     return (0);
+// }
--- src/minishell.c
+++ src/minishell.c
@@ -1,11 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execute.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcamaren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/06 23:12:08 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/07/06 23:12:10 by pcamaren         ###   ########.fr       */
+/*   Created: 2022/07/06 18:52:54 by pcamaren          #+#    #+#             */
+/*   Updated: 2022/07/06 18:54:58 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void    ft_prompt(char *str)
+{
+    printf("minishell:%s$\n", str);
+}
+
+int main(void)
+{
+    int loop;
+    char *env;
+
+    loop = 1;
+    while(loop)
+    {
+        env = getcwd(NULL, 0);
+        ft_prompt(env);
+        exit (1);
+    }
+    return (0);
+}
