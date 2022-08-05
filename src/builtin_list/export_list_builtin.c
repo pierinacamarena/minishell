d2635a859efcedf74a5c885593912f1c397b7bb4
Pierina alessandra CAMARENA PUCUHUANCA <pcamaren@e2r3p9.clusters.42paris.fr>
2022-08-05 21:21:20 +0200

    fixed some export error

--- src/builtin_list/export_list_builtin.c
+++ src/builtin_list/export_list_builtin.c
@@ -6,7 +6,7 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 23:40:21 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/08/05 12:57:16 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/08/05 21:20:03 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,9 +95,41 @@ void	export_check_list(t_shell *shell, char *var)
 	}
 }
 
+int	ft_export_error(char *arg)
+{
+	if (ft_strcmp(arg, "=") == 0)
+	{
+		write_error("export: `=': not a valid identifier");
+		return (1);
+	}
+	else if (arg[0] == '=')
+	{
+		ft_putstr_fd("export: ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": not a valid identifier\n", 2);
+		return (1);
+	}
+	else if (ft_strcmp(arg, "$") == 0)
+	{
+		ft_putstr_fd("export: ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": not a valid identifier\n", 2);
+		return (1);
+	}
+	else if (ft_strcmp(arg, "") == 0)
+	{
+		ft_putstr_fd("export: ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": not a valid identifier\n", 2);
+		return (2);
+	}
+	return (0);
+}
+
 int	ft_export_list(t_shell *shell, t_pipeline *data, int *read_write_fds)
 {
 	int	i;
+	int	error;
 
 	if (array_size(data->command) == 1)
 		ft_env(shell->exp, read_write_fds);
@@ -106,10 +138,14 @@ int	ft_export_list(t_shell *shell, t_pipeline *data, int *read_write_fds)
 		i = 1;
 		while (data->command[i])
 		{
-			if (ft_strcmp(data->command[i], "=") == 0)
-			{
-				write_error("export: `=': not a valid identifier");
+			error = ft_export_error(data->command[i]);
+			if (error == 1)
 				return (1);
+			else if (error == 2)
+			{
+				if (!data->command[i + 1])
+					return (1);
+				i++;
 			}
 			export_check_list(shell, data->command[i]);
 			i++;
