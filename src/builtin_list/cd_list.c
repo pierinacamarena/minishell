253beffd11fb91888264367e612c78e730062aba
Pierina alessandra CAMARENA PUCUHUANCA <pcamaren@e1r1p13.clusters.42paris.fr>
2022-08-05 15:11:03 +0200

    fixed problems with cd

--- src/builtin_list/cd_list.c
+++ src/builtin_list/cd_list.c
@@ -6,7 +6,7 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 23:40:15 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/08/04 23:40:57 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/08/05 15:08:35 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,20 @@ int	len_cmds_list(char **cmds)
 	return (len);
 }
 
-int	cd_helper(t_shell *shell)
+int	cd_helper(t_shell *shell, t_pipeline *data)
 {
 	char	*saved_oldpwd;
 	char	*new_pwd;
 
 	saved_oldpwd = getcwd(NULL, 0);
-	chdir(ft_getenv(shell->env, "HOME"));
+	if (chdir(ft_getenv(shell->env, "HOME")) == -1)
+	{
+		free(saved_oldpwd);
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(data->command[1], 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		return (1);
+	}
 	new_pwd = getcwd(NULL, 0);
 	find_replace(&shell->env, "OLDPWD=", saved_oldpwd);
 	find_replace(&shell->env, "PWD=", new_pwd);
@@ -45,6 +52,7 @@ int	cd_helper_two(t_pipeline *data, t_shell *shell)
 	char	*new_pwd;
 
 	saved_oldpwd = getcwd(NULL, 0);
+	printf("%s\n", data->command[1]);
 	if (chdir(data->command[1]) == -1)
 	{
 		free(saved_oldpwd);
@@ -76,11 +84,11 @@ int	ft_cd_list(t_shell *shell, t_pipeline *data)
 		return (1);
 	}
 	else if (len == 1)
-		return (cd_helper(shell));
+		return (cd_helper(shell, data));
 	else
 	{
 		if (ft_strcmp(data->command[1], "~") == 0)
-			return (cd_helper(shell));
+			return (cd_helper(shell, data));
 		else
 			return (cd_helper_two(data, shell));
 	}
