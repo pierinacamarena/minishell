c9eb3001e3ebe6b61b160323563e4bef23a2f966
Raphael BOURDILLAT <rbourdil@e1r4p20.clusters.42paris.fr>
2022-08-06 21:17:11 +0200

    normed minishell.h, that was something

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,78 +6,77 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:12:55 by rbourdil          #+#    #+#             */
-/*   Updated: 2022/08/06 20:54:30 by rbourdil         ###   ########.fr       */
+/*   Updated: 2022/08/06 21:15:54 by rbourdil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stddef.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stddef.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
 
-#define PROMPT_NAME "minishell% "
+# define PROMPT_NAME "minishell% "
 
 /*	file type definitions */
 
-#define READ_FILE 256
-#define WRITE_FILE 257
-#define HERE_DOC 258
-#define APPEND_FILE 259
-#define SIMPLE_WORD 260
+# define READ_FILE 256
+# define WRITE_FILE 257
+# define HERE_DOC 258
+# define APPEND_FILE 259
+# define SIMPLE_WORD 260
 
 /*	state definitions */
 
-#define QUOTE_STATE 265
-#define DOUBLE_QUOTE_STATE 266
-#define WORD_STATE 267
+# define QUOTE_STATE 265
+# define DOUBLE_QUOTE_STATE 266
+# define WORD_STATE 267
 
 /*  parsing defs */
 
-#define BUFSIZE 512
+# define BUFSIZE 512
 
 /*	token type defs	*/
 
-#define WORD_TOKEN 256
-#define MORE_TOKEN 257
-#define MORE_MORE_TOKEN 258
-#define LESS_TOKEN 259
-#define LESS_LESS_TOKEN 260
-#define PIPE_TOKEN 261
-#define EOF_TOKEN 264
+# define WORD_TOKEN 256
+# define MORE_TOKEN 257
+# define MORE_MORE_TOKEN 258
+# define LESS_TOKEN 259
+# define LESS_LESS_TOKEN 260
+# define PIPE_TOKEN 261
+# define EOF_TOKEN 264
 
 /*	state definitions */
 
-#define QUOTE_STATE 265
-#define DOUBLE_QUOTE_STATE 266
-#define WORD_STATE 267
+# define QUOTE_STATE 265
+# define DOUBLE_QUOTE_STATE 266
+# define WORD_STATE 267
 
-#define PANIC_MODE 1
-#define REGULAR_MODE 0
+# define PANIC_MODE 1
+# define REGULAR_MODE 0
 
 /*	error defs	*/
 
-#define ENV 0x1
-#define CMD 0x2
-#define HIST 0x4
+# define ENV 0x1
+# define CMD 0x2
+# define HIST 0x4
 
 /*	global exit status variable	*/
 
 extern int	g_exit_code;
 
-typedef struct	s_scanner {
+typedef struct s_scanner {
 	const char	*start;
 	const char	*current;
 }	t_scanner;
@@ -125,7 +124,7 @@ typedef struct s_env_list
 	struct s_env_list	*next;
 }				t_env_list;
 
-typedef struct	s_exp_list
+typedef struct s_exp_list
 {
 	char				*content;
 	struct s_exp_list	*next;
@@ -137,13 +136,13 @@ typedef struct s_buffer {
 	char	*collector;
 }	t_buffer;
 
-typedef struct	s_pipes
+typedef struct s_pipes
 {
-	int **fd_pipe;
+	int	**fd_pipe;
 	int	size;
 }				t_pipes;
 
-typedef struct	s_pipex {
+typedef struct s_pipex {
 	t_pipes	pipes;
 	int		rw_fds[2];
 	pid_t	pid;
@@ -158,62 +157,62 @@ typedef struct s_shell
 }				t_shell;
 
 /*minishell*/
-int   	builtin_exec_list(t_pipeline *data, t_shell *shell, int *read_write_fds);
-int     is_builtin_list(t_pipeline *shell);
-int		get_heredoc(char *heredoc);
-int		do_redir(t_elem *redirections);
-void	get_redirs(t_elem *redirections, int *read_write_fds);
+int			builtin_exec_list(t_pipeline *data, t_shell *shell, int *rw_fds);
+int			is_builtin_list(t_pipeline *shell);
+int			get_heredoc(char *heredoc);
+int			do_redir(t_elem *redirections);
+void		get_redirs(t_elem *redirections, int *rw_fds);
 
 /* ppipex_utils */
-int		count_list(t_pipeline *data);
-void	init_pipes(t_pipes *pipes, t_pipeline *data, t_shell *shell);
-void	close_pipes(t_pipes *pipes);
-void	init_fds(t_pipeline *data, t_pipex *pipex, int command_num);
-int		wait_children(int exit_status, pid_t pid, int built_check);
+int			count_list(t_pipeline *data);
+void		init_pipes(t_pipes *pipes, t_pipeline *data, t_shell *shell);
+void		close_pipes(t_pipes *pipes);
+void		init_fds(t_pipeline *data, t_pipex *pipex, int command_num);
+int			wait_children(int exit_status, pid_t pid, int built_check);
 
 /*builtins_list_utils*/
-void	export_check_helper(t_shell *shell, char *var);
+void		export_check_helper(t_shell *shell, char *var);
 
 //int		ft_pwd(void);
-int		len_cmds_list(char **cmds);
-int		ft_pwd(int *read_write_fds);
-int		ft_env(t_env_list *env, int *read_write_fds);
-int		ft_exit_list(t_shell *shell, t_pipeline *data);
-int	    ft_unset_list(t_shell *shell, t_pipeline *data);
-int	    ft_export_list(t_shell *shell, t_pipeline *data, int *read_write_fds);
-int		ft_cd_list(t_shell *shell, t_pipeline *data);
+int			len_cmds_list(char **cmds);
+int			ft_pwd(int *read_write_fds);
+int			ft_env(t_env_list *env, int *read_write_fds);
+int			ft_exit_list(t_shell *shell, t_pipeline *data);
+int			ft_unset_list(t_shell *shell, t_pipeline *data);
+int			ft_export_list(t_shell *shell, t_pipeline *data, int *rw_fds);
+int			ft_cd_list(t_shell *shell, t_pipeline *data);
 
 /* utils */
-int     ft_strcmp(const char *s1, const char *s2);
-size_t	ft_strlen(char const *str);
-char	*ft_strdup(char const *str);
-char	*ft_strchr(char const *str, int c);
-char	*ft_substr(char const *str, unsigned int start, size_t len);
-void	ft_bzero(void *str, size_t n);
-void	*ft_memset(void *str, int character, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-void	ft_free(char **str);
-char	*ft_strjoin(char const *s1, char const *s2);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void    ft_putchar_fd(char c, int fd);
-void    ft_putstr_fd(char *s, int fd);
-void    ft_putendl_fd(char *s, int fd);
-char	*ft_itoa(int n);
-void	full_free(t_shell *shell, t_pipeline *data);
-int		ft_atoi(const char *str);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-void	free_pipes(t_pipes *pipes);
+int			ft_strcmp(const char *s1, const char *s2);
+size_t		ft_strlen(char const *str);
+char		*ft_strdup(char const *str);
+char		*ft_strchr(char const *str, int c);
+char		*ft_substr(char const *str, unsigned int start, size_t len);
+void		ft_bzero(void *str, size_t n);
+void		*ft_memset(void *str, int character, size_t n);
+void		*ft_calloc(size_t count, size_t size);
+void		ft_free(char **str);
+char		*ft_strjoin(char const *s1, char const *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+char		*ft_itoa(int n);
+void		full_free(t_shell *shell, t_pipeline *data);
+int			ft_atoi(const char *str);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+void		free_pipes(t_pipes *pipes);
 
 /* errors */
-void	free_struct(t_shell *shell, t_pipeline *cmds, int opt);
-void	write_error(char *str);
-void	free_exit(int exit_status, t_shell *shell, t_pipeline *cmds, int opt);
-void	print_exit(int exit_status, char *msg);
+void		free_struct(t_shell *shell, t_pipeline *cmds, int opt);
+void		write_error(char *str);
+void		free_exit(int exit, t_shell *shell, t_pipeline *cmds, int opt);
+void		print_exit(int exit_status, char *msg);
 
 /*ft_split*/
 int			ft_wordcount(char const *s, char c);
-char	**ft_split(char const *s, char c);
+char		**ft_split(char const *s, char c);
 
 /*env_setup*/
 t_env_list	*init_env(char **env);
@@ -223,103 +222,103 @@ void		ft_print_list(t_env_list *begin);
 void		ft_free_list(t_env_list **begin);
 
 /*env_for_execute*/
-char	**list_to_array(t_env_list *list);
-void    print_env_array(char **env);
+char		**list_to_array(t_env_list *list);
+void		print_env_array(char **env);
 
 /*export_sort*/
-size_t  array_size(char **arr);
-void    ft_swap(char **a, char **b);
-int     not_sorted(char **env);
-void    print_double_array(char **str);
-char    **sorted_exp(char **env);
+size_t		array_size(char **arr);
+void		ft_swap(char **a, char **b);
+int			not_sorted(char **env);
+void		print_double_array(char **str);
+char		**sorted_exp(char **env);
 
 /*list_utils */
-void    find_replace(t_env_list **list, char *key, char *new_content);
-int		list_size(t_env_list *list);
-int     key_exists(t_env_list *list, char *var);
-int     same_value(t_env_list *list, char *var);
-char    *ft_getenv(t_env_list *begin, char *key);
-char	*ft_str3join(char const *s1, char const *s2, char const *s3);
+void		find_replace(t_env_list **list, char *key, char *new_content);
+int			list_size(t_env_list *list);
+int			key_exists(t_env_list *list, char *var);
+int			same_value(t_env_list *list, char *var);
+char		*ft_getenv(t_env_list *begin, char *key);
+char		*ft_str3join(char const *s1, char const *s2, char const *s3);
 
 /*echo*/
 //int		ft_echo(char **args);
-int		ft_echo(char **args, int *read_write_fds);
+int			ft_echo(char **args, int *read_write_fds);
 
 /*ft_path*/
-char	*ft_path(char *arg, char **env);
-char	*cmd_tester(char **path_split, char *arg);
+char		*ft_path(char *arg, char **env);
+char		*cmd_tester(char **path_split, char *arg);
 
 /*signal*/
-void	sighandler(int signum);
-void	siginit(int signum, void (*sighandler)(int));
+void		sighandler(int signum);
+void		siginit(int signum, void (*sighandler)(int));
 
 /*execute_list*/
-int	exec_pipes(t_pipeline *data, t_shell *shell);
+int			exec_pipes(t_pipeline *data, t_shell *shell);
 
 /*PARSING FUNCTIONS ! :D*/
 
 /*buffer.h*/
 
-void	init_buffer(t_buffer *buffer);
-int		flush(t_buffer *buffer);
-int		add_char_to_buffer(t_buffer *buffer, int c);
-int		add_str_to_buffer(t_buffer *buffer, char *str);
-char	*collect(t_buffer *buffer);
-void	clear_buffer(t_buffer *buffer);
-char	*collect_and_clear(t_buffer *buffer);
+void		init_buffer(t_buffer *buffer);
+int			flush(t_buffer *buffer);
+int			add_char_to_buffer(t_buffer *buffer, int c);
+int			add_str_to_buffer(t_buffer *buffer, char *str);
+char		*collect(t_buffer *buffer);
+void		clear_buffer(t_buffer *buffer);
+char		*collect_and_clear(t_buffer *buffer);
 
 /*expand.h*/
 
-char	*expand_parameters(t_token token, t_shell *shell);
-t_elem	*split_words(t_elem *elem);
+char		*expand_parameters(t_token token, t_shell *shell);
+t_elem		*split_words(t_elem *elem);
 
 /*lexer.h*/
 
-t_token	scan_token(t_scanner *scanner);
+t_token		scan_token(t_scanner *scanner);
 
 /*list.h*/
 
-t_elem	*add_elem_to_list(t_elem *head, t_elem *new_elem);
-t_elem	*copy_elem(t_elem *src);
-void	print_elem_list(t_elem *head);
-void	print_commands_list(t_pipeline *commands_list);
-void	free_elem(t_elem *elem);
-void	free_elem_list(t_elem *elem);
-void	free_commands_list(t_pipeline *pipeline);
+t_elem		*add_elem_to_list(t_elem *head, t_elem *new_elem);
+t_elem		*copy_elem(t_elem *src);
+void		print_elem_list(t_elem *head);
+void		print_commands_list(t_pipeline *commands_list);
+void		free_elem(t_elem *elem);
+void		free_elem_list(t_elem *elem);
+void		free_commands_list(t_pipeline *pipeline);
 
 /*parser.h*/
 
-int	parse(t_scanner *scanner, t_shell *shell);
+int			parse(t_scanner *scanner, t_shell *shell);
 
 /*scanner.h*/
 
-void	init_scanner(t_scanner *scanner, const char *line);
-int		advance(t_scanner *scanner);
-int		peek(t_scanner scanner);
-void	skip_blanks(t_scanner *scanner);
+void		init_scanner(t_scanner *scanner, const char *line);
+int			advance(t_scanner *scanner);
+int			peek(t_scanner scanner);
+void		skip_blanks(t_scanner *scanner);
 
 /*semantics.h*/
 
 t_elem		*new_words_list(t_elem *words_list, t_elem *elem);
-t_elem		*new_elem(t_elem *elem_list, t_token token, int type, t_shell *shell);
+t_elem		*new_elem(t_elem *elem_list, t_token token, int type, t_shell *sh);
 t_pipeline	*new_command(t_pipeline *commands_list, t_elem *words_list);
 
 /*string_ops.h*/
 
-char	*ft_strcpy(char *dest, const char *src);
-char	*ft_strncpy(char *dest, const char *src, size_t n);
-char	*ft_strndup(const char *s, size_t n);
-char	*ft_realloc(char *s, int n);
-char	*append_str(char *s1, const char *s2);
-char	*append_char(char *s1, const char c);
-char	*ft_strcat(char *dest, const char *src);
-char	*ft_strncat(char *dest, const char *src, size_t n);
+char		*ft_strcpy(char *dest, const char *src);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
+char		*ft_strndup(const char *s, size_t n);
+char		*ft_realloc(char *s, int n);
+char		*append_str(char *s1, const char *s2);
+char		*append_char(char *s1, const char c);
+char		*ft_strcat(char *dest, const char *src);
+char		*ft_strncat(char *dest, const char *src, size_t n);
 
 /*utils.h*/
 
-int		ft_isblank(int c);
-int		ft_ismeta(int c);
-int		update_state(int state, int c);
-void	free_double_ptr(char **ptr);
+int			ft_isblank(int c);
+int			ft_ismeta(int c);
+int			update_state(int state, int c);
+void		free_double_ptr(char **ptr);
 
 #endif
