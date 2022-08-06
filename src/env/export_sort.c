654233df168826d1bad6a9caff31b7b51ff21db2
Pierina alessandra CAMARENA PUCUHUANCA <pcamaren@e1r5p21.clusters.42paris.fr>
2022-08-06 20:24:36 +0200

    export sort

--- src/env/export_sort.c
+++ src/env/export_sort.c
@@ -6,30 +6,11 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 20:48:26 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/08/05 14:48:54 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/08/06 20:14:44 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-size_t	array_size(char **arr)
-{
-	int	len;
-
-	len = 0;
-	while (arr[len])
-		len++;
-	return (len);
-}
-
-void	ft_swap(char **a, char **b)
-{
-	char	*temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
 
 int	not_sorted(char **env)
 {
