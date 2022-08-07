cf9f7405064b82df2c911e954c7e2bd2035b5b71
Raphael BOURDILLAT <rbourdil@e1r5p23.clusters.42paris.fr>
2022-08-07 12:45:09 +0200

    removed unused headers

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:12:55 by rbourdil          #+#    #+#             */
-/*   Updated: 2022/08/06 22:56:17 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/08/07 12:43:57 by rbourdil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <limits.h>
-# include <stdbool.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <stddef.h>
 # include <signal.h>
 # include <sys/types.h>
 # include <sys/stat.h>
