bbd6e4776163d10ab7f4f450d6dad43bd189c369
Raphael BOURDILLAT <rbourdil@e1r5p23.clusters.42paris.fr>
2022-08-07 13:58:35 +0200

    fixed leaks on exit

--- src/builtin_list/builtins_list.c
+++ src/builtin_list/builtins_list.c
@@ -6,7 +6,7 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 23:40:11 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/08/07 13:26:55 by rbourdil         ###   ########.fr       */
+/*   Updated: 2022/08/07 13:57:36 by rbourdil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,25 +61,22 @@ int	is_digit_arg(char *s)
 	return (1);
 }
 
-void	exit_helper(t_pipeline *data)
+void	exit_helper(t_pipeline *data, t_shell *shell)
 {
 	int	exit_atoi;
 
 	if (!is_digit_arg(data->command[1]))
 	{
-		free_commands_list(data);
 		write_error("exit: numeric argument required\n");
-		exit(2);
+		free_exit(2, shell, data, ENV | CMD | HIST);
 	}
 	exit_atoi = ft_atoi(data->command[1]);
 	if (data->command[1][0] == '-')
 	{
 		g_exit_code = g_exit_code + exit_atoi;
-		free_commands_list(data);
-		exit(g_exit_code);
+		free_exit(g_exit_code, shell, data, ENV | CMD | HIST);
 	}
-	free_commands_list(data);
-	exit(exit_atoi);
+	free_exit(exit_atoi, shell, data, ENV | CMD | HIST);
 }
 
 int	ft_exit_list(t_shell *shell, t_pipeline *data)
@@ -95,13 +92,8 @@ int	ft_exit_list(t_shell *shell, t_pipeline *data)
 	ft_free_list(&shell->env);
 	ft_free_list(&shell->exp);
 	if (len == 1)
-	{
-		free_commands_list(data);
-		exit(0);
-	}
+		free_exit(0, shell, data, ENV | CMD | HIST);
 	else
-	{
-		exit_helper(data);
-		return (0);
-	}
+		exit_helper(data, shell);
+	return (0);
 }
